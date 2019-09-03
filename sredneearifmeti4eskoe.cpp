022d33e85b71847b03df2afd0641168d2c4ecc43
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-03 15:23:08 +0300

    Add files via upload

---
+++ sredneearifmeti4eskoe.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout << "Enter the numbers: ";
+    cin >> a >> b;
+    c = (a + b) / 2;
+    cout << "Average is "<< c;
+	return 0;
+}
