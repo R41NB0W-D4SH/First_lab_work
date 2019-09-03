d58e05201d7e8fe617fe1b90429d9d05106c5aad
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-03 14:55:34 +0300

    Add files via upload

---
+++ typoploshad.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,s,p;
+	cout << "Enter numbers: ";
+	cin >> a >> b;
+	s = a * b;
+	p = ( a + b ) * 2;
+	cout << "s ="<< s << "p ="<<p;
+	return 0;
+}
