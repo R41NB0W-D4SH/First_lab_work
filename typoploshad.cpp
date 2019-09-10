f0f3f71d76fc9575d76f19aeca612a7c208672dd
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-10 14:27:39 +0300

    Update typoploshad.cpp

--- typoploshad.cpp
+++ typoploshad.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
-	int a,b,s,p;
+	float a, b, s, p;
 	cout << "Enter numbers: ";
 	cin >> a >> b;
+
 	s = a * b;
-	p = ( a + b ) * 2;
-	cout << "s ="<< s << "p ="<<p;
+	p = (a + b) * 2;
+
+	cout << "s = " << s << endl << "p = " << p << endl;
+
+	system("pause");
 	return 0;
 }
