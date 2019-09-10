a22ee73d5d5c49c4bf958d43a845b7ad790ac9dd
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-10 14:43:28 +0300

    Update modyli.cpp

--- modyli.cpp
+++ modyli.cpp
@@ -1,31 +1,26 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
+
 int main()
 {
-    float a,b,c,d,e,f;
-    cout << "Enter the nubmers: ";
-    cin >> a >> b;
-    if (a > 0)
-    {
-        a = a;
-    }
-    else 
-    a = a * (-1);
-    
-    if (b > 0)
-    {
-        b = b;
-    }
-    else 
-    b = b * (-1);
-    
-    c = a + b;
-    d = a - b;
-    e = a * b;
-    f = a / b;
-    cout << "Sum is "<< c <<endl;
-    cout << "Residual is "<< d <<endl;
-    cout << "Multiplication is "<< e <<endl;
-    cout << "Quotient is "<< f <<endl;
+	float a, b, c, d, e, f;
+	cout << "Enter the nubmers: ";
+	cin >> a >> b;
+
+	a = fabs(a);
+	b = fabs(b);
+	c = a + b;
+	d = a - b;
+	e = a * b;
+	f = a / b;
+
+	cout << "Sum is " << c << endl;
+	cout << "Residual is " << d << endl;
+	cout << "Multiplication is " << e << endl;
+	cout << "Quotient is " << f << endl;
+
+	system("pause");
 	return 0;
 }
