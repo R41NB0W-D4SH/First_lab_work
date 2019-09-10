f92f5e66a615a32403a274996afac4321fb62a32
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-10 14:31:02 +0300

    Update dlinaorkyjnosti.cpp

--- dlinaorkyjnosti.cpp
+++ dlinaorkyjnosti.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
-	double pi,l,d;
+	double pi, l, d;
+
 	pi = 3.14;
 	cout << "Enter the diameter: ";
 	cin >> d;
 	l = pi * d;
-	cout << "Circle lenghts is "<<l<<endl;
+	cout << "Circle lenghts is " << l << endl;
+
+	system("pause");
 	return 0;
 }
