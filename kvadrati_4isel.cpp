5d57f2e777353b3a88cb721f68e95daa05e5067e
R41NB0W-D4SH <54800479+R41NB0W-D4SH@users.noreply.github.com>
2019-09-10 14:36:54 +0300

    Update kvadrati_4isel.cpp

--- kvadrati_4isel.cpp
+++ kvadrati_4isel.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
+
 int main()
 {
-    float a,b,c,d,e,f;
-    cout << "Enter the nubmers: ";
-    cin >> a >> b;
-    c = a*a + b*b;
-    d = a*a - b*b;
-    e = (a*a) * (b*b);
-    f = (a*a) / (b*b);
-    cout << "Sum is "<< c <<endl;
-    cout << "Residual is "<< d <<endl;
-    cout << "Multiplication is "<< e <<endl;
-    cout << "Quotient is "<< f <<endl;
+	float a, b, c, d, e, f;
+
+	cout << "Enter the nubmers: ";
+	cin >> a >> b;
+
+	c = pow(a,2) + pow(b,2);
+	d = pow(a, 2) - pow(b, 2);
+	e = pow(a, 2) * pow(b, 2);
+	f = pow(a, 2) / pow(b, 2);
+
+	cout << "Sum is " << c << endl;
+	cout << "Residual is " << d << endl;
+	cout << "Multiplication is " << e << endl;
+	cout << "Quotient is " << f << endl;
+
+	system("pause");
 	return 0;
 }
